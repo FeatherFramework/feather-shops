0524ca5be078149a7b6995d593489902b2234c9d
Apollyon <37031311+JusCampin@users.noreply.github.com>
2026-09-20 01:28:44 -0700

    require Admin policy readiness during startup
    - declare feather-admin as a resource dependency
    - prevent shop provisioning before the service policy provider is installed

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ lua54 'yes'
 name 'feather-shops'
 description 'Authoritative commerce workflows for the Feather Framework'
 author 'Feather Framework'
-version '0.1.3'
+version '0.1.4'
 shops_dev_tests 'false'
 
 client_script 'client/quote_tests.lua'
@@ -28,4 +28,13 @@ server_scripts {
     'server/player_routes.lua'
 }
 
-dependencies { 'oxmysql', 'feather-core', 'feather-economy', 'feather-organizations', 'feather-inventory', 'feather-toolkit', 'feather-menu-v2' }
+dependencies {
+    'oxmysql',
+    'feather-core',
+    'feather-economy',
+    'feather-organizations',
+    'feather-inventory',
+    'feather-toolkit',
+    'feather-menu-v2',
+    'feather-admin'
+}
